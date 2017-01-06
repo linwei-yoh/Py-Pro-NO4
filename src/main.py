d65f367d179505818204f63170d53a099888af85
linwei-yoh <395373035@qq.com>
2017-01-06 16:50:42 +0800

    准备分离所有人

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ tar_excel_path = '../output.xls'
 
 
 # 所有人对象 包含所有人名称 和 所有专利集合
-class patentOwner(object):
+class OwnerItem(object):
     def __init__(self, name):
         self.name = name
         self.patentset = set()
@@ -18,7 +18,7 @@ class patentOwner(object):
 
 
 # 专利对象 包含专利名称 和 引用专利集合
-class patentItem(object):
+class PatentItem(object):
     def __init__(self, name):
         self.name = name
         self.refset = set()
@@ -31,10 +31,10 @@ owner_dict = {}  # 所有人字典
 patent_dict = {}  # 专利字典
 unique_name_dict = {}  # 专利唯一名称字典
 
-# 1.逐个读取各个专利 获取专利名称 和 专利所有人
+# 1.逐个读取各个专利 获取专利名称(唯一） 和 专利所有人
 # 2.查询当前专利所有人是否已经在所有人字典中存在 否，则创建并添加 是,则添加专利名称到其专利集合中
 # 3.遍历完成，获得所有人字典 记载了全部所有人，及其所拥有的专利
-# 4.逐个读取各个专利 获取专利名称 和 引用专利
+# 4.逐个读取各个专利 获取专利名称(唯一） 和 引用专利(转化为唯一专利）
 # 5.查询引用专利是否在专利字典中存在 否，则创建并添加 是,则添加专利名称到引用专利的被引用专利集合中
 # 6.遍历完成，获得专利被引用字典 记载了全部被引用专利，及其所引用该专利的专利集合
 # 7.逐个遍历所有人字典，对单个所有人的全部专利的被引用集合作并集处理，获得对此所有人的引用
@@ -44,13 +44,13 @@ unique_name_dict = {}  # 专利唯一名称字典
    name_owner  专利所有人名"""
 
 
-def addPatentToOwner(name_patent, name_owner):
+def addPatentToOwner(name_patent, ownerset):
     global owner_dict
-    if name_owner not in owner_dict:
-        new_owner = patentOwner(name_owner)
-        owner_dict[name_owner] = new_owner
-
-    owner_dict[name_owner].addToPatentset(name_patent)
+    for owner_name in ownerset:
+        if owner_name not in owner_dict:
+            new_owner = OwnerItem(owner_name)
+            owner_dict[owner_name] = new_owner
+        owner_dict[owner_name].addToPatentset(name_patent)
 
 
 """name_ref 被引用的专利
@@ -66,8 +66,12 @@ def addRefToPatent(name_patent, name_ref):
     patent_dict[name_ref].addToRefset(name_patent)
 
 
-def get_ref_unique_name(namelist,unique_name):
-    pass
+def get_ref_unique_name(nameset, unique_name):
+    global unique_name_dict
+    for name_item in nameset:
+        if name_item not in unique_name_dict:
+            unique_name_dict[name_item] = unique_name
+
 
 def getCentreDegree():
     global owner_dict
@@ -104,12 +108,17 @@ def calOwnerCentreDegree():
     # 建立两个字典
     for i in range(1, num_rows):
         lineArray = table.row_values(i)
-        namelist = lineArray[subname_index].split(';')
-        namelist = [item.strip() for item in namelist]
 
-        get_ref_unique_name(namelist,lineArray[patent_index])
-        addPatentToOwner(lineArray[patent_index], lineArray[owner_index])
-        addRefToPatent(lineArray[patent_index], lineArray[ref_index]) # 需要将被引用的专利名称改为唯一专利名
+        # 建立唯一专利号字典
+        namelist = lineArray[subname_index].split(';')
+        nameset = set([item.strip() for item in namelist])
+        get_ref_unique_name(nameset, lineArray[patent_index])
+
+        # 建立所有人字典
+        ownerlist = lineArray[owner_index].split(';')
+        ownerset = set([item.strip() for item in ownerlist])
+        addPatentToOwner(lineArray[patent_index], ownerset)
+        addRefToPatent(lineArray[patent_index], lineArray[ref_index])  # 需要将被引用的专利名称改为唯一专利名
 
     # 获得中心度
     getCentreDegree()
